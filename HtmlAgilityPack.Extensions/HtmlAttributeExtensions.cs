8b511f7d399aeae3a8f1616ee7c955b95e94b45f
Mikael Dúi Bolinder <github@bolinder.uk>
2022-01-16 01:51:29 +0100

    Updated assembly name.

--- HtmlAgilityPack.Extensions/HtmlAttributeExtensions.cs
+++ HtmlAgilityPack.Extensions/HtmlAttributeExtensions.cs
@@ -15,7 +15,7 @@ namespace HtmlAgilityPack
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
             // Unix timestamp is seconds past epoch
-            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            DateTime dateTime = new(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
             return dateTime;
         }
@@ -23,7 +23,7 @@ namespace HtmlAgilityPack
         public static DateTime JavaTimeStampToDateTime(double javaTimeStamp)
         {
             // Java timestamp is milliseconds past epoch
-            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            DateTime dateTime = new(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             dateTime = dateTime.AddMilliseconds(javaTimeStamp).ToLocalTime();
             return dateTime;
         }
